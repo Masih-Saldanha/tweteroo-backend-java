223cf67675b882204593f591c0e47025c5f81e7d
Masih <masih.saldanha@gmail.com>
2023-01-22 19:42:17 -0300

    fix: fix some CORS and path errors

--- src/main/java/com/tweteroo/api/controller/SignUpController.java
+++ src/main/java/com/tweteroo/api/controller/SignUpController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,8 @@ import com.tweteroo.api.service.EndUserService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/sign-up")
+@RequestMapping("/api/auth/sign-up")
+@CrossOrigin(origins = "*")
 public class SignUpController {
     @Autowired
     private EndUserService service;
--- src/main/java/com/tweteroo/api/controller/TweetsController.java
+++ src/main/java/com/tweteroo/api/controller/TweetsController.java
@@ -3,12 +3,15 @@ package com.tweteroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweeteDTO;
@@ -17,12 +20,14 @@ import com.tweteroo.api.service.TweeteService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/tweets")
+@RequestMapping("/api/tweets")
+@CrossOrigin(origins = "*")
 public class TweetsController {
     @Autowired
     private TweeteService service;
 
     @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
     public String createTweet(@RequestBody @Valid TweeteDTO req) {
         service.createTweet(req);
         return "OK";
